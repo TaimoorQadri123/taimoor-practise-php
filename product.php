28a82a58d5c2fe47c743348b841ed58d51145b38
TaimoorQadri123 <dchota374@gmail.com>
2025-01-24 21:01:10 -0800

    class work done

--- product.php
+++ product.php
@@ -1,168 +1,22 @@
 <?php 
-include('components/header.php');
 include('php/query.php');
-
+include('components/header.php');
 ?>
-		
-
-	<!-- Slider -->
-	<section class="section-slide">
-		<div class="wrap-slick1">
-			<div class="slick1">
-				<div class="item-slick1" style="background-image: url(images/slide-01.jpg);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Women Collection 2018
-								</span>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									NEW SEASON
-								</h2>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
-								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="item-slick1" style="background-image: url(images/slide-02.jpg);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Men New-Season
-								</span>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Jackets & Coats
-								</h2>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="item-slick1" style="background-image: url(images/slide-03.jpg);">
-					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Men Collection 2018
-								</span>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
-								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									New arrivals
-								</h2>
-							</div>
-								
-							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
-								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-
-	<!-- Banner -->
-	<div class="sec-banner bg0 p-t-80 p-b-50">
-		<div class="container">
-			<div class="row">
-		
-			   <?php 
-			       $query = $pdo->query("Select * from categories limit 3");
-				   $allcategories = $query->fetchAll(PDO::FETCH_ASSOC);
-				   foreach($allcategories as $category){
-
-				   
-			   ?>
-                    
-				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<!-- Block1 -->
-					<div class="block1 wrap-pic-w">
-				<img src="dashmin/img/categories/<?php echo $category['image']?>" alt="IMG-BANNER">
-
-	<a href="product.php?cId=<?php echo $category['image']?>" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-								<?php echo $category['name']?>
-								</span>
-
-								<span class="block1-info stext-102 trans-04">
-									Spring 2018
-								</span>
-							</div>
-
-							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Shop Now
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-                       
-				<?php 
-				}
-				?>
-				
-			</div>
-		</div>
-	</div>
-
-
+	
 	<!-- Product -->
-	<section class="bg0 p-t-23 p-b-140">
+	<div class="bg0 m-t-23 p-b-140">
 		<div class="container">
-			<div class="p-b-10">
-				<h3 class="ltext-103 cl5">
-					Product Overview
-				</h3>
-			</div>
-
 			<div class="flex-w flex-sb-m p-b-52">
 				<div class="flex-w flex-l-m filter-tope-group m-tb-10">
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
+					<!-- <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
 						All Products
-					</button>
-					<?php 
-					$query = $pdo->query("Select * from categories");
-					$allcategories = $query->fetchAll(PDO::FETCH_ASSOC);
-					foreach($allcategories as $category){
+					</button> -->
 
-					
-					?>
-
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".<?php echo $category['catId']?>">
-						<?php echo $category['name']?>
+					<!-- <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
+						Women
 					</button>
 
-					<?php 
-					}
-					?>
-
-					<!-- <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
 						Men
 					</button>
 
@@ -179,7 +33,7 @@ include('php/query.php');
 					</button> -->
 				</div>
 
-				<div class="flex-w flex-c-m m-tb-10">
+				<!-- <div class="flex-w flex-c-m m-tb-10">
 					<div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
 						<i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
 						<i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
@@ -191,10 +45,10 @@ include('php/query.php');
 						<i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
 						Search
 					</div>
-				</div>
+				</div> -->
 				
 				<!-- Search product -->
-				<div class="dis-none panel-search w-full p-t-10 p-b-15">
+				<!-- <div class="dis-none panel-search w-full p-t-10 p-b-15">
 					<div class="bor8 dis-flex p-l-15">
 						<button class="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
 							<i class="zmdi zmdi-search"></i>
@@ -202,10 +56,10 @@ include('php/query.php');
 
 						<input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Search">
 					</div>	
-				</div>
+				</div> -->
 
 				<!-- Filter -->
-				<div class="dis-none panel-filter w-full p-t-10">
+				<!-- <div class="dis-none panel-filter w-full p-t-10">
 					<div class="wrap-filter flex-w bg6 w-full p-lr-40 p-t-27 p-lr-15-sm">
 						<div class="filter-col1 p-r-15 p-b-27">
 							<div class="mtext-102 cl2 p-b-15">
@@ -391,16 +245,24 @@ include('php/query.php');
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>
 
 			<div class="row isotope-grid">
-			<?php
-				$query = $pdo->query("select * from products");
-				$allProducts = $query->fetchAll(PDO::FETCH_ASSOC);
-						foreach($allProducts  as $product){
-				?>
-				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item <?php echo $product['product_cat_id']?>">
+				
+                  <?php 
+				  if(isset($_GET['cId'])){
+					$categoryId = $_GET['cId'];
+					$query = $pdo->prepare("select * from products where product_cat_id = :cId");
+					$query->bindParam('cId',$categoryId);
+					$query->execute();
+					$allproducts = $query->fetchAll(PDO::FETCH_ASSOC);
+					foreach($allproducts as $product){
+				  
+				  ?>
+
+
+				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
 					<!-- Block2 -->
 					<div class="block2">
 						<div class="block2-pic hov-img0">
@@ -418,7 +280,7 @@ include('php/query.php');
 								</a>
 
 								<span class="stext-105 cl3">
-								$<?php echo $product['product_price']?>
+								<?php echo $product['product_price']?>
 								</span>
 							</div>
 
@@ -432,10 +294,12 @@ include('php/query.php');
 					</div>
 				</div>
 
-				<?php
+				<?php 
+
+                                  }
+
 				}
 				?>
-
 				
 			</div>
 
@@ -446,8 +310,8 @@ include('php/query.php');
 				</a>
 			</div>
 		</div>
-	</section>
-
-<?php 
+	</div>
+		
+	<?php 
 include('components/footer.php');
 ?>
